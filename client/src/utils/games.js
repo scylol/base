7c0fb94f1d3bf97b018d270eb841b3b7cdc22114
Dakota Bryant <bryant.dakota@gmail.com>
2017-07-26 06:53:53 -0500

    Fixed selector components

--- client/src/utils/games.js
+++ client/src/utils/games.js
@@ -1,19 +1,19 @@
 const games = [
   {
-    title: 'PC',
-    image: 'http://exosphere.sphere3d.com/wp-content/uploads/2014/09/Windows_logo_Cyan_rgb_D1.png',
+    title: 'Dota 2',
+    image: 'https://s-media-cache-ak0.pinimg.com/originals/2d/cd/80/2dcd80c6f5a21a437313adde93b373d8.jpg',
   },
   {
-    title: 'PS4',
-    image: 'http://i.imgur.com/nWSNWSc.png'
+    title: 'League of Legends',
+    image: 'https://vignette1.wikia.nocookie.net/leagueoflegends/images/1/12/League_of_Legends_Icon.png/revision/latest?cb=20150402234343'
   },
   {
-    title: 'Xbox',
-    image: 'http://i.imgur.com/JJYdkHz.png'
+    title: 'Overwatch',
+    image: 'http://i.imgur.com/YZ4w2ey.png'
   },
   {
-    title: 'Switch',
-    image: 'http://i.imgur.com/VTkU521.png'
+    title: 'Diablo 3',
+    image: 'https://s-media-cache-ak0.pinimg.com/originals/1d/ae/1a/1dae1ad263fbac22a9296014871cb980.png'
   }
 ];
 
