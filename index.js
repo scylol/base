4d2e87db5f08ecef979fec5bb6fc8b2cc4028bab
BAAMOSK <bunth667@gmail.com>
2017-08-04 00:20:24 -0400

    added undefined to run server

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ if (process.env.NODE_ENV === 'production') {
   //   res.sendFile(path.resolve(__dirname + 'client', 'build', 'index.html'));
   // });
     // Just run the server
-  runServer(process.env.PORT || 8080);
+
+  runServer(undefined, process.env.PORT || 8080);
 }
 else {
   const app = express();
