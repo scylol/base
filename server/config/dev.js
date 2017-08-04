6e5f5ae181b101ac5e029ba1f88472f1f33617d0
BAAMOSK <bunth667@gmail.com>
2017-08-03 21:00:48 -0400

    deploying to heroku

---
+++ server/config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  PORT: 3001,
+  CLIENT_ID: '181732598757-v3i89rldnqe3k7107fd653fhs5udfi3g.apps.googleusercontent.com',
+  CLIENT_SECRET: '-j2BKeEpTwEq_sk0LQGitDXv',
+  DATABASE_URL:'mongodb://scylol:1234@ds149511.mlab.com:49511/base'
+};
