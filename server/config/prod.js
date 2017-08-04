2d51bf9ba6f376c7cef73c38240ccdbdc653c424
BAAMOSK <bunth667@gmail.com>
2017-08-03 23:10:07 -0400

    deleted config folder

--- server/config/prod.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  PORT: process.env.PORT,
-  CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-  CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-  DATABASE_URL: process.env.MONGO_URI
-}
